a7ccde48ba4584129798f07e09e7820a607993e5
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-09-07 04:32:00 +0500

    Included frontend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use('/api/v1/transactions', transactions);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`.green.bold);
